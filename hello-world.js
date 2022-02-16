91f01302c73d67cf593b0bf4f386b0218e451485
Joilton Macedo <joiltonmacedo2022@outlook.com>
2022-02-16 17:48:29 -0300

    Alterações do meu primeiro Hello World

---
+++ hello-world.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+console.log("Hello, Ton");
+console.log("Quebrando a cabeça episódio 1");
+console.log("quebrando a cabeça episódio 2 a missão");
+const nome = "Joilton";
+const apelido = "ton";
+
+
+
+
